344db11a953d2365fb744688977c089b722568f5
Ashutosh Yadav <ashutosh_yadav@Ashutoshs-MacBook-Air.local>
2024-04-29 10:34:41 +0530

    ..

--- mongoose_models/models/ecommerce-models/catrgory.models.js
+++ mongoose_models/models/ecommerce-models/catrgory.models.js
@@ -5,7 +5,7 @@ const categorySchema = new mongoose.Schema({
     name:{
         type: String,
         required: true
-    }
+    },
 
 }, {timestamp:true})
 
--- mongoose_models/models/ecommerce-models/order.model.js
+++ mongoose_models/models/ecommerce-models/order.model.js
@@ -1,4 +1,3 @@
-import { ref } from 'firebase/database'
 import mongoose from 'mongoose'
 
 
